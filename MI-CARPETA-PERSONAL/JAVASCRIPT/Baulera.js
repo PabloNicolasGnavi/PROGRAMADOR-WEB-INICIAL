e868e34d19d5099cc46d137bbbfd6cc419e0b11a
Pablo Gnavi <pablonicolasgnavi@gmail.com>
2026-05-14 21:01:31 -0300

    Commit 2026/05/14 21:01

---
+++ MI-CARPETA-PERSONAL/JAVASCRIPT/Baulera.js
@@ -0,0 +1,8 @@
+/* RECORREDOR DE DIAS */
+const dias = ['lunes','martes','miercoles','jueves','viernes']
+
+for (let i = 0; i<10; i++){
+    for (let a = 0; a < 5; a++)
+        if (i==a){console.log(`Hoy es ${dias[a]}`)}
+
+}
