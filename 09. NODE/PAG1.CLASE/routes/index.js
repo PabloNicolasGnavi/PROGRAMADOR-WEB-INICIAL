bf35cc60f46c41fda568d5864760953e99056d6c
Pablo Gnavi <pablonicolasgnavi@gmail.com>
2026-06-16 00:05:23 -0300

    Nuevos Commits

--- 09. NODE/PAG1.CLASE/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pablo82' });
-});
-
-module.exports = router;
